ae5f6fb7d9840a9249ceb0f04cde866bb5f4ac3f
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-07-18 11:05:05 -0500

    Added functionality on ProductTypeDetails

--- Bangazon/Controllers/ProductTypesController.cs
+++ Bangazon/Controllers/ProductTypesController.cs
@@ -34,6 +34,7 @@ namespace Bangazon.Controllers
             }
 
             var productType = await _context.ProductType
+                .Include(pT => pT.Label)
                 .FirstOrDefaultAsync(m => m.ProductTypeId == id);
             if (productType == null)
             {
