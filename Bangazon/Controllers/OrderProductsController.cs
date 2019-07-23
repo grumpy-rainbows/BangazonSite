cc25b496fe743db9965ce38cc208176763e0ec93
selam <selam0315@gmail.com>
2019-07-23 10:03:12 -0500

    redirect the adding order to cart view

--- Bangazon/Controllers/OrderProductsController.cs
+++ Bangazon/Controllers/OrderProductsController.cs
@@ -107,7 +107,7 @@ namespace Bangazon.Controllers
                 await _context.SaveChangesAsync();
             }
 
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index", "Orders");
 
             
         }
